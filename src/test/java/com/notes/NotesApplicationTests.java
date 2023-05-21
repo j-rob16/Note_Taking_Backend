0d9ad11765a0c2093fc933b0137a29d4616266fb
James R <jimmer1609@gmail.com>
2023-05-21 17:22:30 +0900

    one more test added

--- src/test/java/com/notes/NotesApplicationTests.java
+++ src/test/java/com/notes/NotesApplicationTests.java
@@ -1,13 +1,33 @@
 package com.notes;
 
+import com.notes.models.Note;
+import com.notes.repositories.NoteRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class NotesApplicationTests {
 
+	@Autowired
+	NoteRepository noteRepository;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void createNewNote() {
+		List<Note> initialNotes = noteRepository.findAll();
+		assertEquals(2, initialNotes.size());
+		Note testNote = new Note("Unit Test", "Test if this note can be saved to the repository");
+		noteRepository.save(testNote);
+		List<Note> allNotes = noteRepository.findAll();
+		assertEquals(3, allNotes.size());
 	}
 
 }
